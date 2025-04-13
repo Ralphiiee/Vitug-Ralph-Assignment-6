1c357fd592cbaa3cfd349d98bf914ecd6f002f85
Ralph.Vitug <rralph@rrc.ca>
2025-04-13 13:17:43 -0500

    Defining Enum class of MortgageRate to contain interest rates for each term. PaymentFrequency to contain the type or payment per anum.

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -12,5 +12,33 @@ periods are used.
 
 from enum import Enum
 
+class MortgageRate(Enum): #initialing a class to contain the interest rate method
+    """ MortgageRate consists of valid interest rate that corresponds the 
+    type of mortgage
+
+    FIXED rate is lower because it will not be affected by the market
+    within the agreed upon duration of the contract.
+
+    VARIABLE rate is higher because it follows the market's volatility,
+    if the entire market is doing good, then the interest drops 
+
+    """
+    FIXED_5 = 0.0519
+    FIXED_3 = 0.0589
+    FIXED_1 = 0.0599
+    VARIABLE_5 = 0.0649
+    VARIABLE_3 = 0.0669
+    VARIABLE_1 = 0.679
+
+class PaymentFrequency(Enum): #initializing a class to contain the method for payment frequency
+    """ PaymentFrequent class represents the frequency of payment.
+    MONTHLY will assign payment for each month of the year
+    BI_WEEKLY will assign payment twice, each month to a total of 26 payments per year
+    WEEKLY will assign payment every week to a total of 52 payments per year
+    """
+    MONTHLY = 12
+    BI_WEEKLY = 26
+    WEEKLY = 52
+    
 VALID_AMORTIZATION = {5, 10, 15, 20, 25, 30}
 
